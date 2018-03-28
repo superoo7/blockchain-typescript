97065dddda67178a7e71ad02bb1c7fdd68b20701
superoo7 <jlwhoo7@gmail.com>
2018-03-28 19:14:31 +0800

    Done setting up express route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import express = require('express');
+
 import Block from './block';
 import BlockChain from './blockchain';
 import Transaction from './transaction';
@@ -8,19 +10,30 @@ interface TransactionData {
     amount: number;
 }
 
+// API
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send("hello world");
+})
+
+app.get('/blockchain', function(req, res) {
+    let transaction : TransactionData = new Transaction('Han', 'Yan', 12);
+
+    let genesisBlock = new Block();
+    let blockchain = new BlockChain(genesisBlock);
+
+    let block = blockchain.getNextBlock([transaction]);
+    blockchain.addBlock(block);
+
+    let anotherTransaction: TransactionData = new Transaction("tester1", "tester2", 10);
+    let block1 = blockchain.getNextBlock([anotherTransaction, transaction]);
+
+    res.json(blockchain);
+})
+
+app.listen(3000, function() {
+    console.log('server started');
+})
 
 
-let transaction : TransactionData = new Transaction('Han', 'Yan', 12);
-
-let genesisBlock = new Block();
-let blockchain = new BlockChain(genesisBlock);
-
-let block = blockchain.getNextBlock([transaction]);
-blockchain.addBlock(block);
-
-let anotherTransaction: TransactionData = new Transaction("tester1", "tester2", 10);
-let block1 = blockchain.getNextBlock([anotherTransaction, transaction]);
-
-console.log(blockchain);
-console.log(blockchain.blocks[blockchain.blocks.length-1].transactions);
-console.log(block1.transactions);
